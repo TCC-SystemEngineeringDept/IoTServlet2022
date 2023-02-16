237fe7c25adfb1c4924b1fd841a278497681a29d
皓大 菅野 <200064@jc-21.jp>
2023-02-16 16:46:11 +0900

    最新にしました。

--- src/IoTServlet2.java
+++ src/IoTServlet2.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -11,52 +9,65 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /**
  * Servlet implementation class IoTServlet
  */
 @WebServlet("/iot2")
 public class IoTServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
 		PrintWriter out = response.getWriter();
 		ServletContext application = this.getServletContext();
-		 ArrayList<String> list = (ArrayList<String>)application.getAttribute("list");
+		ArrayList<String> list = (ArrayList<String>) application.getAttribute("list");
 		 if(list==null) {
 			 return;
 		 }
-
-		 for(String s :list) {
-			out.println("<H3>"+s+"</H3>");
-		 }
-		 
-
+		for (String s : list) {
+			System.out.println("<H3>");
+			System.out.println(s);
+			System.out.println("</H3>");
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String host = request.getHeader("host");
-		System.out.println("host["+host+"]");
-
+		System.out.println("host[" + host + "]");
 		String user = request.getHeader("user");
-		System.out.println("user["+user+"]");
-
+		System.out.println("user[" + user + "]");
 		String content = request.getReader().readLine();
 		System.out.println(content);
-		
-		
 		ServletContext application = this.getServletContext();
+		ArrayList<String> list = (ArrayList<String>) application.getAttribute("list");
+		if (list == null) {
+			list = new ArrayList<String>();
+			
 
-		 ArrayList<String> list = (ArrayList<String>)application.getAttribute("list");
-		 if(list == null) {
-			 list = new ArrayList<String>();
-		 }
+		}
 		 list.add("host["+host+"] user["+user+"] content=["+content+"]");
 		 application.setAttribute("list", list);
-		
-		
+
+		Gson gson = new Gson();
+		Content result = gson.fromJson(content, Content.class);
+		if (result.roll >= 170) {
+				response.getWriter().append("GREEN");
+		} else if (result.roll <= 5) {
+				response.getWriter().append("RED");
+		} else {
+				response.getWriter().append("NONE");
+		}
 		
 	}
+}
+
+class Content {
+	float roll;
+	float pitch;
 
 }
